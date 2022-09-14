de7155cbb7d37bf2126d9c0f6e5419cbe12d65da
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2022-09-14 18:41:44 +0300

    add recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipes.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A pie', 'This is my pie', 'https://i.pinimg.com/originals/93/5b/e0/935be0854f8f419df9770fe8ee7c2b40.jpg')
+    new Recipe('A pie', 'This is my pie', 'https://i.pinimg.com/originals/93/5b/e0/935be0854f8f419df9770fe8ee7c2b40.jpg'),
+    new Recipe('Meat', 'This is my shashlik', 'https://i.pinimg.com/originals/3e/88/d6/3e88d6849458773c5a6d71b7f89e5b66.jpg')
   ];
 
   constructor() { }
