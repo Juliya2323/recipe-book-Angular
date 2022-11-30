8d28f4f240dba48006308dd225349366de40238f
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2022-11-30 17:19:57 +0300

    preparing the sugnup request

---
+++ src/app/auth/auth-service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  RefreshToken: string;
+  expiresin: string;
+  localId: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string, ) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDe7NuCbFnECQ9Q_rLoljK_mPcl2PWwlzU',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+      );
+  }
+}
