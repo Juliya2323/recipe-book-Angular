cac0b388d20f4fb33cc744ec701ff4ce0fa2d2ea
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2022-12-04 16:52:49 +0300

    add token with interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -26,29 +26,20 @@ export class DataStorageService {
     //take operator: take 1 value from our observable and then unsubscribe
     //delete subscribe, move setRecipes to TAP operator and subscribe in header-component in fetch method
 
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http.get<Recipe[]>
-        (
-          'https://course-project-377b9-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-          {
-            params: new HttpParams().set('ayth', user.token)
-          }
-        );
-    }),
-      map(recipes => {
-      return recipes.map(recipe => {
-        return {
-          ...recipe,
-          ingredients: recipe.ingredients ? recipe.ingredients : []
-        };
-      });
-    }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-      })
-    );
+    return this.http.get<Recipe[]>
+        ('https://course-project-377b9-default-rtdb.europe-west1.firebasedatabase.app/recipes.json').pipe(
+          map(recipes => {
+            return recipes.map(recipe => {
+              return {
+                ...recipe,
+                ingredients: recipe.ingredients ? recipe.ingredients : []
+              };
+            });
+          }),
+            tap(recipes => {
+              this.recipeService.setRecipes(recipes);
+            })
+        )
   }
 
 
