03cab8d56e4f451c7f6c37204aadeaa5fffdf801
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2022-12-09 13:42:32 +0300

    splitting modules(add recipesModule)

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipesComponent } from "./recipes.component";
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeStartComponent,
+        RecipeEditComponent,
+    ],
+    exports: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeStartComponent,
+        RecipeEditComponent,
+    ],
+    imports: [RouterModule, CommonModule, ReactiveFormsModule]
+    //добавить browserModule можно только 1 раз!!!! тут добавили CommonModule для ngIf / ngFor
+    // модули работают отдельно, поэтому некоторые импортсы нужно добавлять отдельно. это не касается серсисов
+})
+export class RecipesModule {
+
+}
