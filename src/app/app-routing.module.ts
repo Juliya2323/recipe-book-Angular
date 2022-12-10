3ac2d5a85600356662bb64950a2302de8f870132
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2022-12-10 15:05:01 +0300

    add lazy loading (auth, shopping-list)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRroutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(module => module.RecipesModule) }
+  {path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(module => module.RecipesModule) },
+  {path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(module => module.ShoppingListModule) },
+  {path: 'recipes', loadChildren: () => import('./auth/auth.module').then(module => module.AuthModule) }
 ];
 
 @NgModule({
